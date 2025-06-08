892ba8818f9c9589dc09fd7f018cd21d7e5c1d91
Meerim Nurbekova <marynurbekova@gmail.com>
2025-06-08 22:42:07 +0000

    task3

---
+++ session2/hometask/task3.py
@@ -0,0 +1,6 @@
+password = input("Enter your password: ")
+
+if (len(password) >= 8 and "@" in password and " " not in password):
+    print("The password is strong")
+else:
+    print("The password is weak")
