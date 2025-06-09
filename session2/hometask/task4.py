1fabd80d8434d4b4518583f12fcae853049c5ef6
Meerim Nurbekova <marynurbekova@gmail.com>
2025-06-09 23:03:05 +0000

    minot fix

--- session2/hometask/task4.py
+++ session2/hometask/task4.py
@@ -5,7 +5,7 @@ player2 = input("Please enter rock/paper/scissors:" )
 
 if player1 == "rock" and player2 == "scissors":
     print("Player1 wins")
-elif player1 == "scissor" and player2 == "paper":
+elif player1 == "scissors" and player2 == "paper":
     print("Player1 wins")
 elif player1 == "rock" and player2 == "paper":
     print("Player2 wins")
