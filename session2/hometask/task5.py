5f42fa2d3573afa6828e6b24e5c3ef1ee5f50325
Meerim Nurbekova <marynurbekova@gmail.com>
2025-06-09 23:14:42 +0000

    fix

--- session2/hometask/task5.py
+++ session2/hometask/task5.py
@@ -6,9 +6,10 @@ c = float(input("Please enter the length of side 3: "))
 if (a + b > c) and (a + c > b) and (b + c > a):
   if a == b == c:
     print("Equilateral triangle")
-    elif a == b or b == c or a == c:
+  elif a == b or b == c or a == c:
     print("Isosceles triangle")
-    else:
+  else:
     print("Scalene triangle")
+    
 else:
     print("Not a triangle")
