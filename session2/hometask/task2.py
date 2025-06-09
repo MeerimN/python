e72576a06c0e7615868342636441c4dc6ea568f2
Meerim Nurbekova <marynurbekova@gmail.com>
2025-06-09 23:20:39 +0000

    fix

--- session2/hometask/task2.py
+++ session2/hometask/task2.py
@@ -3,8 +3,9 @@ inp2 = input("Enter second Boolean (True/False): ")
 
 and_result = inp1 and inp2
 or_result = inp1 or inp2
-not_result = not inp1
+not_result = not inp1 
+not_result = not inp2
 
 print(f"{inp1} and {inp2} = {and_result}")
 print(f"{inp1} or {inp2} = {or_result}")
-print(f"not {inp1} = {not_result}")
+print(f"not {inp1} not {inp2} = {not_result}")
