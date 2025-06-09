602f8f0c3b8070ef7a6d89425766f17a96da3bf4
Meerim Nurbekova <marynurbekova@gmail.com>
2025-06-09 22:28:36 +0000

    task4

---
+++ session2/hometask/task4.py
@@ -0,0 +1,21 @@
+player1 = input("Please enter rock/paper/scissors:" )
+
+player2 = input("Please enter rock/paper/scissors:" )
+
+
+if player1 == "rock" and player2 == "scissors":
+    print("Player1 wins")
+elif player1 == "scissor" and player2 == "paper":
+    print("Player1 wins")
+elif player1 == "rock" and player2 == "paper":
+    print("Player2 wins")
+elif player1 == "scissors" and player2 == "rock":
+    print("Player2 wins")
+elif player1 == "paper" and player2 == "rock":
+    print("Player1 wins")
+elif player1 == "paper" and player2 == "scissor":
+    print("Player2 wins")
+elif player1 == player2:
+    print("It's a tie")
+else:
+    print("Invalid input")
