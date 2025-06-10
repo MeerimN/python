e6df463abd83d3041c5091e9a2fae410789eadc6
Meerim Nurbekova <marynurbekova@gmail.com>
2025-06-10 00:51:03 +0000

    new folder

---
+++ session3/classtask/session3.py
@@ -0,0 +1,89 @@
+# for loop until 5:
+# print
+
+# iterator = variable used within loop  the value of which changes based on the sequence
+# sequence data type = data type that has start point and end point
+# string support indexing through indexing we can get the start of the string and end
+
+# for <iterator> in <sequence_data type>
+# <code that needs to be repeated>
+
+# inp = input("Input: ")
+# Input (each character we access through index)
+# for char in inp:
+    # print (char)
+
+
+# inp = input("Input Word: ")
+# inp2 = input("Input a Character: ")
+# 
+# for char in inp:
+    # if char == inp2:
+       # print(char)
+
+
+# inp = input("Input Word: ")
+# inp2 = input("Input a Character: ")
+# 
+# counter = 0
+# for char in inp:
+    # if (char) == inp2:
+       # counter += 1 
+# 
+# print(counter)
+
+# for num in range (1, 10, 2):  # <start> <end> <jump>
+# 
+    # print(num)    
+
+# inp = int(input("Input: "))
+# 
+# for num in range (inp):
+    # if num % 2 == 0:
+        # print(num)
+
+# While loops
+# unlike for loop, while does not have a start or end
+# is based on conditions
+# if the condition is true: code will be repeated until it turns False.
+
+# while <condition>: num1 < num2
+   # <code>
+
+# inp = int(input("Input: "))
+# num = 0
+# while num < inp:
+#     print(num)
+#     num += 1
+
+# break / continue - used with loops only
+# break - stops the loop
+# 
+
+# for i in range (1, 10):
+#     if i == 8:
+#         break
+#     else:
+#         print(i)   
+
+# for i in range (1, 10):
+#        if i == 5:
+#           continue
+#        else:
+#           print(i) 
+                  
+
+range_num = int(input("Range: "))
+break_point = int(input("Break: "))
+
+if range_num < break_point:
+    print("Out of range")
+else:
+    num = 0
+    while num < range_num:
+        if break_point 
+    print(num)
+    num += 1
+
+
+# Lists     
