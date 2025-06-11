9341f3fb5b10686d46f6f8b7f7b18e452509e127
Meerim Nurbekova <marynurbekova@gmail.com>
2025-06-11 23:15:18 +0000

    folder

---
+++ session4/session4.py
@@ -0,0 +1,17 @@
+# lst = [1,2,3]
+# del lst[0]
+# print(lst)
+
+# lst = [2,3,1,-1,0,-40,5]
+
+# lst = ["Hello", "Hello", "World"]
+# print(lst.index("Hello"))
+
+# Split (only works with strings) split() (default is ' ')
+# str1 = "Hello World. This is a test"
+# lst_str = str1.split()
+# print(lst_str)
+
+str1 = " Hello, World, This, is a test, "
+x = str1.strip()
+print(x)
